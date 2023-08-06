60c9492957b85ae58350296804383858c7fcade1
VladM <vdstories20@gmail.com>
2023-08-06 23:14:30 +0200

    basic logic for shopping list implemented

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import * as categories from './js/categories.js';
-import * as logOut from './js/logOut.js';
-import * as suport_ukraine from './js/support_Ukraine.js';
-import * as header from './js/header.js';
+// import * as categories from './js/categories.js';
+// import * as logOut from './js/logOut.js';
+// import * as suport_ukraine from './js/support_Ukraine.js';
+// import * as header from './js/header.js';
+import * as shopping_list from './js/shopping-list.js';
